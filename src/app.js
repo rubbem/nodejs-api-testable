89b156cb2209a286222dcb7bbf76169aee255440
EC2 Default User <ec2-user@ip-172-31-45-14.us-east-2.compute.internal>
2019-03-22 19:32:11 +0000

    First commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+ const app = express();
+ app.use(bodyParser.json());
+
+ app.get('/products', (req, res) => res.send([{
+ name: 'Default product',
+ description: 'product description',
+ price: 100
+ }]));
+
+export default app;
